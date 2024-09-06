33e95c1c4173c932360d2ac2d85aa6936fc27d72
LucaWilliams4831 <lucaw4831@gmail.com>
2024-09-06 19:06:09 +0300

    handler banner, file

---
+++ backend/handler/fileUpload.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"atro/internal/helper"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+//FileUploadHandler -> Interface to File Upload
+type FileUploadHandler interface {
+	SingleFile(*gin.Context)
+}
+
+//SingleFile --> handle uploading of single file
+func SingleFile(c *gin.Context) {
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		fmt.Println("get form file "+ err.Error())
+		c.JSON(http.StatusInternalServerError, helper.BuildResponse(-1, "gửi lên tao có thấy gì đâu ?", err.Error()))
+		return
+	}
+
+	nameFile := uuid.NewString()
+
+	savePath := os.Getenv("IMAGE_SAVE_PATH")
+	log.Println(file.Filename + "save at: " + savePath)
+
+	extSplit := strings.Split(file.Filename, ".")
+	ext := extSplit[len(extSplit)-1]
+
+	err = c.SaveUploadedFile(file, savePath+nameFile+"."+ext)
+	if err != nil {
+		fmt.Println("save err" + err.Error())
+		c.JSON(http.StatusInternalServerError, helper.BuildResponse(-1, "lấy dc ảnh nhưng lỗi cmnr khi savefile", err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, helper.BuildResponse(1, "get file done", "https://atroboticsvn.com/static/images/"+nameFile+"."+ext))
+
+}
